1f51691f0ff28b56ae1d676f7390b7bd9f1ef150
DedeCahyadiKurniansyah <115217601+DedeCahyadiKurniansyah@users.noreply.github.com>
2022-10-23 21:53:15 +0800

    Add files via upload

---
+++ 8.6 Praktikum (5).py
@@ -0,0 +1,8 @@
+print("MENGHITUNG LINGKARAN")
+print("-------------------------------")
+r=int(input("Masukkan jari-jari = "))
+luas=3.14*r*r
+print("Luas Lingkaran = ", luas)
+print("-------------------------------")
+
+ 
