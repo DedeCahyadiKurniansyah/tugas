1d90a81b1ebd996fa99b5bfa175c89f37d3d6246
DedeCahyadiKurniansyah <115217601+DedeCahyadiKurniansyah@users.noreply.github.com>
2022-10-23 21:52:21 +0800

    Add files via upload

---
+++ 8.6 Praktukim (4).py
@@ -0,0 +1,9 @@
+print("MENGHITUNG LUAS SEGI TIGA ")
+print("-------------------------------")
+a=int(input("Masukkan Alas = "))
+t=int(input("Masukkan tinggi = "))
+luas=(1/2)*a*t
+print("Luas Segi Tiga = ", luas)
+print("-------------------------------")
+
+ 
